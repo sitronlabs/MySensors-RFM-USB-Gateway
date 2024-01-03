bffcf67851dfee3a79dc1b30d90bb6bb22fd728d
Nicolas Schurando <nicolas@sitronlabs.com>
2024-01-03 21:54:57 +0100

    Enabled watchdog.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 /* Config */
 #include "../cfg/config.h"
 
-/* Arduino Libraries */
+/* Arduino libraries */
 #include <Arduino.h>
 #include <MySensors.h>
 
+/* Avr libraries */
+#include <avr/wdt.h>
+
 void setup() {
+
+    /* Enable watchdog to prevent the gateway from becoming unresponsive */
+    wdt_enable(WDTO_8S);
 }
 
 void presentation() {
 }
 
 void loop() {
+
+    /* There is no need to explicitely pet the watchdog here
+     * as MySensors does it before every call of the loop function */
 }
